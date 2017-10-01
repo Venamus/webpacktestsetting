e8ba158ee4acfa5b9fa5d93c2e954544923d77bf
leonardo gutierrez <lgutierrez@flukyfactory.com>
2017-09-30 19:17:54 -0600

    75% ON MESSAGES

--- src/index.js
+++ src/index.js
@@ -10,22 +10,25 @@ $(document).ready( function() {
 
 		$(".tables-container").append($generatedTable);
 	});
-	$("body").on('click','.table-btn', function() {
+	$("body").on('click','.row-insertedComment', function() {
+		const $parentTable = $(this).parent().parent();
+		const $tableHeader = $parentTable.parent().parent().find('.panel-heading').text();
+		const $infoTable = `<div class="col-sm-4 col-md-3 table-info-container"><div class="panel panel-default"><div class="panel-heading">Tables Info</div><div class="panel-body"><table class="info-holder"><th>${"Name of table: "+$tableHeader}</th></table><table class="info-holder"><th>Comments</th></table></div><div class="panel-footer"><input type="text" class="form-control add-tableRow table-input" placeholder="Enter new Comment..."><button class="btn btn-primary btn-block table-btn">Enter new Comment</button></div></div></div>`;
+
+		if ($infoTable.length) {
+				return console.log("hola")
+		} if($infoTable.length === 0 ){
+			$("body").append($infoTable);
+		}
+	});
+	$("body").on('click','.btn', function() {
 		const $parentTable = $(this).parent().parent();
 		const rowValue = $parentTable.find(".table-input").val();
 		const $selectedTable = $parentTable.find(".items-holder");
+		const $selectedInfo = $parentTable.find(".info-holder");
 
-		$selectedTable.append(`<tr><td>${rowValue}</td></tr>`)
-	});
-	$("body").on('click','tr', function() {
-		const $parentTable = $(this).parent().parent();
-		const $selectedTable = $parentTable.find(".items-holder");
-		$selectedTable.find($(this)).after('<div class="row-comment"><td><input type="text" class="form-control " placeholder="Enter new Comment..."></div>');
-	});
-	$("body").on("focusout",'.row-comment', function() {
-		$(this).addClass("not-active");
-	});
-	$("body").on("focus",'.row-comment', function() {
-		$(this).removeClass("not-active");
+		$selectedInfo.append(`<tr><td class="row-insertedComment">${rowValue}</td></tr>`);
+		$selectedTable.append(`<tr><td class="row-insertedComment">${rowValue}</td></tr>`);
+
 	});
 });
